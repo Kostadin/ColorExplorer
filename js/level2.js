11255975bb0eaaac2e68caa222a097baec03bec4
jagblade <jagblade@ABV.BG>
2014-01-25 19:57:02 +0200

    Level 2

---
+++ js/level2.js
@@ -0,0 +1,19 @@
+levels[1]={
+        width:15360,
+        height:600,
+        player: {
+                x:0,
+                y:0,
+                color:0
+        },
+        tiles:[
+//Reds
+		{
+                type: platform,
+                x:0,
+                y:100,
+                width:128,
+                height:20,
+                color:0,
+                texture:'img/brick0.jpg'
+        },
