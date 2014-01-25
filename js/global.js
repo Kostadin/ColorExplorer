72e58b095fdbd99a43712970c624ff6a2ecf143d
Kostadin Vodenicharov <kostadinvo@gmail.com>
2014-01-25 11:12:29 +0200

    Small stuff

--- js/global.js
+++ js/global.js
@@ -10,7 +10,7 @@ var gameRunning = false;
 var colors=['red','yellow','green','blue'];
 var currentColor = 0;
 var currentColorCSS = colors[currentColor];
-var playSound = true;
+var playSound = false;
 //Brick types
 var platform = 0;
 var portal = 1;
--- js/sounds.js
+++ js/sounds.js
@@ -1,7 +1,7 @@
 //Audio objects
 var backgroundTrack = new Audio("audio/Test.mp3");
 //Looping
-testTrack.addEventListener('ended', function() {
+backgroundTrack.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
