593fa76127d0feedab4012a6cc616e69ba5c13bf
blurymind <blurymind@gmail.com>
2014-01-27 23:17:16 +0200

    graphics

--- js/main.js
+++ js/main.js
@@ -319,7 +319,7 @@ function shuffle(array) {
 }
 
 $(function(){
-	var credits = ['Kostadin Vodenicharov - Dev', 'Neven Iliev - Dev', 'Todor Imreorov -  Art and Animation' , 'Ilko Birov - Music', 'Jordan Panov - Concept Level Design'];
+	var credits = ['Kostadin Vodenicharov - Dev', 'Neven Iliev - Dev', 'Todor Imreorov -  Graphics' , 'Ilko Birov - Music', 'Jordan Panov - Concept Level Design'];
 	shuffle(credits);
 	var logstr = 'Credits\n-------\n';
 	for (var i=0;i<credits.length;++i){
